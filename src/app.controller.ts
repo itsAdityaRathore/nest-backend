19224dd2b89c9db5b8b43f480ffb7a24f623f339
Aditya Rathore <adityarathore35@gmail.com>
2023-09-10 13:35:45 +0530

    added query param

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 import { OtpDTO } from './app.otp';
 
@@ -15,4 +15,15 @@ export class AppController {
   getOtp(@Body() body: OtpDTO) {
     return this.appService.getOtp(body);
   }
+
+  @Get("verify-otp")
+  verifyOtp(@Query() params: any) {
+    return this.appService.verifyOtp(params.otp);
+  }
+
+  @Get("user/:otp")
+  verifyMyOtp(@Param() params: any) {
+    return this.appService.verifyOtp(params.otp);
+  }
+  
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -7,9 +7,21 @@ export class AppService {
     return '{"Hello World!": "How are you"}';
   }
 
+  otparray = [2];
+
   getOtp(body: OtpDTO): any {
     var otp = Math.floor(100000 + Math.random() * 900000);
+    this.otparray.push(otp);
     body.otp = otp;
     return body;
+  }
+
+  verifyOtp(userOtp: Number) {
+
+    var otp = this.otparray.find((e) => e == userOtp);
+    if (otp == userOtp) {
+      return '{"status":"user verified"}';
+    }
+    return '{"status":"user not verified"}';
   }
 }
